8b3018d5fafdb957fd5aff6b7a72cd4913fae073
nooch2327 <g0627857667@gmail.com>
2024-08-07 17:45:22 +0700

    first commit

--- src/app/addProduct/page.js
+++ src/app/addProduct/page.js
@@ -43,18 +43,42 @@ export default function AddProduct() {
   return (
     <>
       <div className="flex justify-between items-center">
-        <h1 className="font-bold py-10 text-2xl">Add New Product</h1>
+        <h1 className="font-bold py-10 text-2xl mx-20">Add New Product</h1>
       </div>
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <input
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          className="input input-bordered input-accent w-full max-w-xs"
-          type="text"
-          placeholder="Product Name"
-        />
-
-        <input
+      <div className="mt-10 grid grid-cols-1 gap-x-12 gap-y-8 sm:grid-cols-10 ml-24">
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="first-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      ยุทธศาสตร์ชาติ
+    </label>
+    <div className="mt-2">
+      <input
+      onChange={(e) => setName(e.target.value)}
+      value={name}
+      className="input input-bordered input-accent w-full max-w-xs"
+      type="text"
+      placeholder="Product Name"
+    />
+        {/* type="text"
+        name="first-name"
+        id="first-name"
+        autoComplete="given-name"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+     */}
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="first-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      โครงงาน
+    </label>
+    <div className="mt-2">
+    <input
           onChange={(e) => setImage(e.target.value)}
           value={image}
           className="input input-bordered input-accent w-full max-w-xs"
@@ -62,7 +86,23 @@ export default function AddProduct() {
           placeholder="/images/1.jpg"
           defaultValue="/images/1.jpg"
         />
-        <input
+        {/* type="text"
+        name="first-name"
+        id="first-name"
+        autoComplete="given-name"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+     */}
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="first-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      หน่วยงานที่นำนโยบายไปปฏิบัติ
+    </label>
+    <div className="mt-2">
+    <input
           onChange={(e) => setPrice(e.target.value)}
           value={price}
           className="input input-bordered input-accent w-full max-w-xs"
@@ -70,15 +110,196 @@ export default function AddProduct() {
           placeholder="1"
           defaultValue="1"
         />
-        <input
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      อีเมล
+    </label>
+    <div className="mt-2">
+    <input
           onChange={(e) => setCategory(e.target.value)}
           value={category}
           className="input input-bordered input-accent w-full max-w-xs"
           type="text"
           placeholder="Product Category"
         />
+      
+        
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      โทรศัพท์
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+      
+        
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+    งบประมาณ(ล้านบาท)  
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+      
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      ปีงบประมาณ
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+      
+        
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      ผลประเมิน
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      จุดเเข็ง
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      จุดอ่อน
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      เเนวทางเสริมจุดเเข็ง
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      ข้อที่ควรปรับปรุงจุดอ่อน
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+    </div>
+  </div>
+  <div className="sm:col-span-3">
+    <label
+      htmlFor="last-name"
+      className="block text-sm font-medium leading-6 text-gray-900"
+    >
+      ข้อเสนอเเนะ
+    </label>
+    <div className="mt-2">
+    <input
+          onChange={(e) => setCategory(e.target.value)}
+          value={category}
+          className="input input-bordered input-accent w-full max-w-xs"
+          type="text"
+          placeholder="Product Category"
+        />
+      
+        
+    
+      
+        
+    </div>
+  </div>
+</div>
 
-        <button type="submit" className="btn btn-primary w-full max-w-xs">
+
+
+        
+        <button type="submit" className="btn btn-primary w-full max-w-xs mx-20">
           Add Product
         </button>
       </form>
